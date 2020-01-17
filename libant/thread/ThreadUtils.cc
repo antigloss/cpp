912869b346a6e138a6190367b6ef41ecaca718fc
Antigloss <antigloss@163.com>
2020-01-17 09:17:50 +0800

    threadpool

---
+++ libant/thread/ThreadUtils.cc
@@ -0,0 +1,19 @@
+#include "ThreadUtils.h"
+
+void ThreadBlockAllSignals()
+{
+#ifndef _WIN32
+	sigset_t mask;
+	if (sigfillset(&mask) != 0) {
+		int err = errno;
+		LOG(WARNING) << "sigfillset failed! err=" << strerror(err);
+		return;
+	}
+
+	int err = pthread_sigmask(SIG_BLOCK, &mask, nullptr);
+	if (err != 0) {
+		LOG(WARNING) << "pthread_sigmask failed! err=" << strerror(err);
+		return;
+	}
+#endif // !_WIN32
+}
---
+++ libant/thread/ThreadUtils.h
@@ -0,0 +1,7 @@
+#ifndef LIBANT_THREADUTILS_H_
+#define LIBANT_THREADUTILS_H_
+
+// 线程屏蔽所有信号
+void ThreadBlockAllSignals();
+
+#endif // LIBANT_THREADUTILS_H_
